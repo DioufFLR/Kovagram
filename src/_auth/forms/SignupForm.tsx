e0b1c46fe6cdb93dac9a0fcb4d2a9854f49e2ce8
diouf <geoffreyfleur@gmail.com>
2023-12-07 19:40:12 +0100

    :ukraine: Add signup form and loader, and update CSS and form validation rules
    This commit introduces a signup form along with a loader displayed during the sign-up process. Additionally, it includes considerable updates to CSS styling and makes adjustments to the form validation rules. These changes together enhance the usability and the visual appeal of the application.

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,26 +1,31 @@
-import * as z from 'zod';
 import {zodResolver} from "@hookform/resolvers/zod"
+import {Link} from "react-router-dom";
 import {useForm} from "react-hook-form"
-import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
 import {Input} from "@/components/ui/input.tsx";
 import {Button} from '@/components/ui/button';
+import {SignupValidtaion} from "@/lib/validation";
+import {z} from "zod";
+import Loader from "@/components/shared/Loader.tsx";
 
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-})
 
 const SignupForm = () => {
 
+    const isLoading = false;
+
     // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof SignupValidtaion>>({
+        resolver: zodResolver(SignupValidtaion),
         defaultValues: {
+            name: '',
             username: "",
+            email: '',
+            password: ''
         },
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    function onSubmit(values: z.infer<typeof SignupValidtaion>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values)
@@ -28,9 +33,26 @@ const SignupForm = () => {
 
 
     return (
-        <div>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <Form {...form}>
+            <div className='sm:w-420 flex-center flex-col'>
+                <img src="/assets/images/logo.svg" alt="logo"/>
+
+                <h2 className='h3-bold md:h2-bold pt-5 sm:pt-12'>Create a new account</h2>
+                <p className='text-light-3 small-medium md:base-regular mt-2'>To use Kovagram, please enter your details</p>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input type='text' className='shad-input' {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
                     <FormField
                         control={form.control}
                         name="username"
@@ -38,19 +60,53 @@ const SignupForm = () => {
                             <FormItem>
                                 <FormLabel>Username</FormLabel>
                                 <FormControl>
-                                    <Input placeholder="shadcn" {...field} />
+                                    <Input type='text' className='shad-input' {...field} />
                                 </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
                                 <FormMessage/>
                             </FormItem>
                         )}
                     />
-                    <Button type="submit">Submit</Button>
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input type='email' className='shad-input' {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input type='password' className='shad-input' {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className='shad-button_primary'>
+                        {isLoading ? (
+                            <div className='flex center gap-2'>
+                                <Loader />Loading...
+                            </div>
+                        ) : 'Sign up'
+                        }
+                    </Button>
+                    <p className='text-small-regular text-light-2 text-center mt-2'>
+                        Already have an account?
+                        <Link to='/sign-in' className='text-primary-500 text-small-semibold ml-1'>Log in</Link>
+                    </p>
                 </form>
-            </Form>
-        </div>
+            </div>
+        </Form>
     );
 };
 
