33d999662960a09a6b7a12ef5ff68041d71cfd00
diouf <geoffreyfleur@gmail.com>
2023-12-11 14:25:20 +0100

    :ukraine: Implement search feature in Explore and detail view in PostDetails
    Added a search input field in the Explore page, allowing users to search for posts. Enhanced PostDetails page to include post details, navigation aids, and related post displays. Implemented delete post functionality, and fetching specific post details using react-query.
    
    Signed-off-by: diouf <geoffreyfleur@gmail.com>

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,7 +1,25 @@
+import {Input} from "@/components/ui/input.tsx";
+import {useState} from "react";
+
 const Explore = () => {
+
+    const [searchValue, setSearchValue] = useState('')
+
     return (
-        <div>
-            Explore
+        <div className='explore-container'>
+            <div className="explore-inner_container">
+                <h2 className='h3-bold md:h2-bold w-full'>Search Posts</h2>
+                <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+                    <img src="/assets/icons/search.svg" alt="search" width={24} height={24}/>
+                    <Input
+                        type='text'
+                        placeholder='Search'
+                        className='explore-search'
+                        value={searchValue}
+                        onChange={(event) => setSearchValue(event.target.value)}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
