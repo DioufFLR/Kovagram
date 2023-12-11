d0747ae8d9a5040c69f28137afe87cda703692b8
diouf <geoffreyfleur@gmail.com>
2023-12-11 12:42:19 +0100

    :ukraine: Fix typo in route path
    The route path for 'EditPost' had a typo, reading '/updte-post/:id' instead of '/update-post/:id'. This commit fixes that typo to ensure the correct routing.

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
                     <Route path='/saved' element={<Saved />} />
                     <Route path='/all-users' element={<AllUsers />} />
                     <Route path='/create-post' element={<CreatePost />} />
-                    <Route path='/updte-post/:id' element={<EditPost />} />
+                    <Route path='/update-post/:id' element={<EditPost />} />
                     <Route path='/posts/:id' element={<PostDetails />} />
                     <Route path='/profile/:id/*' element={<Profile />} />
                     <Route path='/update-profile/:id' element={<UpdateProfile />} />
